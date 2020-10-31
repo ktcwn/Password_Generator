1a22ba148b059f065c8f40c1fcb87dfd61eb384a
Katelynn Cowan <kcowan@twu.edu>
2020-10-31 12:03:56 -0500

    added if and else statements to ensure the user inputs the correct information:

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerLetter = ["a","b","c","d","e","f","g","h","i","j",'k',"l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-for (var i = 0; i < lowerLetter.length; i++) {
-  console.log(lowerLetter[i]);
-}
 
 // Variables for Arrays
+var lowerLetter = ["a","b","c","d","e","f","g","h","i","j",'k',"l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upperLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 var symbols = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","<",">","."];
-var passwordString = ""
 
+// User prompts and confirms for password generator
 var howMany = prompt("How many characters do you want?")
 var wantLowerCase = confirm("Do you want lower case?")
 var wantUpperCase = confirm("Do you want upper case?")
 var wantNumbers = confirm("Do you want numbers?")
 var wantLowerCase = confirm("Do you want lower case?")
 
-// // generatePassword Function 
-// function generatePassword() {
-//   for (var i = 0; i < lowerLetter.length; i++) {
+// generatePassword Function 
+function generatePassword() {
+for (var i = 0; i < lowerLetter.length; i++) {
     
-//   
 
 // Write password to the #password input
 function writePassword() {
@@ -34,3 +30,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  }
+}
